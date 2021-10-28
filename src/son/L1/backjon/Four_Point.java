2ccd16108dd2b7fceba4d18457427298729e1ca9
SonGeon <geons.dev@gmail.com>
2021-10-28 22:35:25 +0900

    아... 문제를 덥어써 버렸다....

--- src/son/L1/backjon/Four_Point.java
+++ src/son/L1/backjon/Four_Point.java
@@ -3,49 +3,35 @@ package son.L1.backjon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * 백준 네번째 점
- * https://www.acmicpc.net/problem/3009  
+ * https://www.acmicpc.net/problem/4153
  */
 public class Four_Point {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Map<Integer, Integer> count = new HashMap<>();
-
-        for(int i =0; i< 3; i++){
+        while (true){
             StringTokenizer st = new StringTokenizer(br.readLine());
+            List<Integer> arr = new ArrayList<>();
+            arr.add(Integer.parseInt(st.nextToken()));
+            arr.add(Integer.parseInt(st.nextToken()));
+            arr.add(Integer.parseInt(st.nextToken()));
 
-            for(int j = 0; j<2; j++){
-                int key = Integer.parseInt(st.nextToken());
-                if(count.containsKey(key )){
-                    int val = count.get(key);
-                    count.replace(key, val+1);
-                }else{
-                    count.put(key,1);
-                }
+            Collections.sort(arr);
+
+            if(arr.get(0) == 0 && arr.get(1) == 0 && arr.get(2) == 0){
+                break;
+            }
+
+            if(Math.sqrt(Math.pow(arr.get(0),2) + Math.pow(arr.get(1),2)) == arr.get(2) ){
+                System.out.println("right");
+            }else{
+                System.out.println("wrong");
             }
         }
-
-        for (int key : count.keySet()) {
-            if(count.get(key) != 2){
-                System.out.println(count.get(key));
-            }
-        }
-
-
-
-
-
-
-
-
-
-
     }
 }
